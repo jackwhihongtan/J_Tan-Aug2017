959f2288f6eea3b398dbf4775f7c385a4a83fc4f
jackwhihongtan <18tanj98@millshighsch.org>
2017-08-20 07:52:28 -0700

    I tried my Best on these changes they might be wrong

---
+++ Hello_World/Hello_World.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author Butterpick
+ *
+ */
+public class Hello_World {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
