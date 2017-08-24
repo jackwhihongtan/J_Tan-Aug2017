c2537e98fc29b631f64377540377640b08589d3b
jackwhihongtan <18tanj98@millshighsch.org>
2017-08-24 09:30:33 -0700

    These are your bonono files Always add to index before you commit Compares Commit and push always teh right choice

---
+++ Project2B/src/Project2B.java
@@ -0,0 +1,13 @@
+//Jack Tan
+/* August 24, 2018
+  This class compares output using print and println
+ */
+public class Project2B {
+
+	public static void main(String[] args) {
+		System.out.println("Hello");
+		System.out.print("Ni hao, wo jiao tan wai xiong");
+
+	}
+
+}
